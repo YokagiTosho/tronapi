7ff714693b35a85bc843bd0338c31f22648c0cb4
YokagiTosho <sandascend@gmail.com>
2025-08-08 18:40:48 +0300

    Add tron api calls

--- src/tronapi/main.py
+++ src/tronapi/main.py
@@ -1,16 +1,42 @@
 from fastapi import FastAPI
-from tronpy import Tron
+
+from tronpy import AsyncTron
+from tronpy.providers import AsyncHTTPProvider
+
+from dotenv import dotenv_values
+
+from typing import Optional
+
+config = dotenv_values(".env")
+
+API_KEY: Optional[str] = config.get("API_KEY")
+if API_KEY is None:
+    print("API KEY is not specified")
+    exit(1)
 
 app = FastAPI()
 
-tron = Tron()
+tron_provider = AsyncHTTPProvider(api_key=API_KEY)
+
 
 @app.get("/get_last_records/")
-def get_last_records(page: int = 1):
+async def get_last_records(page: int = 1):
     limit = 10
-    return {'offset': (page-1)*limit, 'limit': limit}
+    return {"offset": (page - 1) * limit, "limit": limit}
 
 
 @app.post("/wallet/")
-def wallet_information(address: str):
-    return {'address': address}
+async def wallet_information(address: str):
+    async with AsyncTron(tron_provider) as client:
+        energy = await client.get_energy(address)
+        bandwidth = await client.get_bandwidth(address)
+        balance = await client.get_account_balance(address)
+
+        return {
+            "address": address,
+            "account_info": {
+                "energy": energy,
+                "bandwidth": bandwidth,
+                "trx balance": balance,
+            },
+        }
